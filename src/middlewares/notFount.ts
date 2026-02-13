4a445457348fcdf0406a3bb4d0f0cfd32553b0c6
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2026-02-14 03:32:05 +0600

    Organized files into Backend folder

--- src/middlewares/notFount.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-export function notFount (req:Request,res:Response){
-res.status(404).json({
-   message:"Route not found!",
-   path:req.originalUrl,
-   date: Date()
-})
-}
